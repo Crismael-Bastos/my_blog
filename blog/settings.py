2512c5d3740f0f2bef617dcb57fedc99d7f908f7
Crismael Bastos <crismaelbastos@gmail.com>
2022-07-13 20:27:19 -0300

    DEBUG TURN ON

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-  'posts',
+    'posts',
     'categories',
     'comments',
     'django.contrib.admin',
